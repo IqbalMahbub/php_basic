2182525229b5abecccef4ab954f15be737c7e74f
iqbal <iqbalmahbub420@gmail.com>
2024-11-01 11:14:01 +0600

    Initial commit

---
+++ form.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Idcard</title>
+</head>
+<body>
+    <form action="id.pphp"  method="post">
+        <h1>Create Your Virtual Id card</h1>
+        <div class="form-field">
+            <section>
+            <label for="fild" class="label-required">Name</label>
+             <input name="std_name" type="text" placeholder="Your name">
+            </section>
+            <section>
+            <label for="fild" class="label-required">Name</label>
+            <input name="std_name" type="text" placeholder="Your name">
+            </section>
+          
+        </div>
+        
+
+    </form>
+</body>
+</html>
